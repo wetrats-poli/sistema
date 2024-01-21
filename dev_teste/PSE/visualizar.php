f7bb97b7ad1a03b72dd4e4ddbb965670dadade68
DanielCozzubo <42482898+DanielCozzubo@users.noreply.github.com>
2024-01-20 22:13:43 -0300

    Updating PSE (#6)

--- dev_teste/PSE/visualizar.php
+++ dev_teste/PSE/visualizar.php
@@ -48,11 +48,8 @@
                     <?php
                         echo '<table style="margin-left: 40px; width:95%" class="tabela_prep">';
                         $link = mysqli_connect("srv976.hstgr.io", "u418844475_wtr", "Wetrats2019", "u418844475_wtr");
-                
-                        $todos_ids = array();
-
                         
-                        $sql2 = 'SELECT id, apelido FROM usuarios WHERE nivel != 2 AND id != 31 AND ativo != 0 ORDER BY apelido ASC ';
+                        $sql2 = 'SELECT id, apelido FROM usuarios WHERE nivel != 2 AND ativo != 0 ORDER BY apelido ASC ';
                         $ids = mysqli_query($link, $sql2);
                         while($id = mysqli_fetch_assoc($ids)){
                             echo '<tr class="tabela_prep"><td class="tabela_prep"><h3 style="color:white; font-size:24px; text-align:center">'.$id['apelido'].'</h3>
@@ -66,12 +63,10 @@
                                         <canvas id="myChart_rat_'.$id['id'].'" height="14%" width="90%"></canvas>
                                     </div>
                                     </td></tr>';
-                            $todos_ids[] = $id['id'];
                         }
                         
                         echo '</table>';
-                    
-                            
+                
                     ?>
                 </div>
             </div>
@@ -100,92 +95,49 @@
     <?php
         date_default_timezone_set('America/Sao_Paulo');
         $hoje=date("Y-m-d");
-    
-        include '../preparacao_fisica/prog_semanas.php';
-        $semana_atual = acha_semana($hoje);
 
-        if(date('l', strtotime($hoje)) == "Monday"){
-            $dia_s = 1;
-        }elseif (date('l', strtotime($hoje)) == "Tuesday") {
-            $dia_s = 2;
-        }elseif (date('l', strtotime($hoje)) == "Wednesday") {
-            $dia_s = 3;
-        }elseif (date('l', strtotime($hoje)) == "Thursday") {
-            $dia_s = 4;
-        }elseif (date('l', strtotime($hoje)) == "Friday") {
-            $dia_s = 5;
-        }elseif (date('l', strtotime($hoje)) == "Saturday") {
-            $dia_s = 6;
-        }elseif (date('l', strtotime($hoje)) == "Sunday") {
-            $dia_s = 7;
-        }
-
-        $link = mysqli_connect("srv976.hstgr.io", "u418844475_wtr", "Wetrats2019", "u418844475_wtr");
-        $sql2 = "SELECT data FROM treinos WHERE id >= 238 AND total>0";
+        $sql2 = "
+            SELECT 
+                td.id_atleta AS ATLETA,
+                GROUP_CONCAT(DATE_FORMAT(td.data_treino, '%d/%m') ORDER BY td.data_treino) AS DATAS,
+                GROUP_CONCAT(COALESCE(pse.ses, 0) ORDER BY td.data_treino) AS PSE_SES,
+                GROUP_CONCAT(COALESCE(pse.descs, 0) ORDER BY td.data_treino) AS PSE_DESCS,
+                GROUP_CONCAT(COALESCE(ROUND(pse.ratio, 2), 0) ORDER BY td.data_treino) AS PSE_RATIO
+            FROM (
+                SELECT 
+                    t.data AS data_treino,
+                    t.id as id_treino,
+                    u.id as id_atleta
+                FROM treinos t
+                CROSS JOIN usuarios u
+                WHERE t.id >= 573
+                AND t.data <= ".$hoje."
+                AND u.nivel != 2
+                AND u.ativo != 0
+            ) AS td
+            LEFT JOIN pse_2024 pse
+                ON td.id_treino = pse.id_treino
+                AND td.id_atleta = pse.id_atleta
+            GROUP BY 1
+            ORDER BY 1;
+        ";
         $res2 = mysqli_query($link, $sql2);
+        $pse_data = mysqli_fetch_assoc($res);
 
-        foreach($todos_ids as $_id){
-            $ses = array();
-            $descs = array();
-            $ratio = array();
-            for($sem = 1; $sem <= $semana_atual; $sem++){   
-                if($sem != $semana_atual){
-                    for($dia = 1; $dia <= 7; $dia++){
-                        $sqlt = "SELECT id FROM treinos WHERE semana=$sem AND dia_semana=$dia AND total>0 AND id >= 238 LIMIT 1";
-                        if(mysqli_num_rows(mysqli_query($link, $sqlt))>0){
-                            $sql = "SELECT ses, descs, ratio FROM pse_2022 WHERE id_atleta=$_id AND semana=$sem AND dia_semana=$dia";
-                            $qry = mysqli_query($link, $sql);
-                            if(mysqli_num_rows($qry)>0){
-                                $res = mysqli_fetch_assoc($qry);
-
-                                $ses[] = $res['ses'];
-                                $descs[] = $res['descs'];
-                                $ratio[] = $res['ratio'];
-                            }
-                            else{
-                                $ses[] = 0;
-                                $descs[] = 0;
-                                $ratio[] = 0;
-                            }
-                        }
-                    }
-                }
-                else{   
-                    for($dia = 1; $dia <= $dia_s; $dia++){
-                        $sqlt = "SELECT id FROM treinos WHERE semana=$sem AND dia_semana=$dia AND total>0 LIMIT 1";
-                        if(mysqli_num_rows(mysqli_query($link, $sqlt))>0){
-                            $sql = "SELECT ses, descs, ratio FROM pse_2022 WHERE id_atleta=$_id AND semana=$sem AND dia_semana=$dia";
-                            $qry = mysqli_query($link, $sql);
-                            if(mysqli_num_rows($qry)>0){
-                                $res = mysqli_fetch_assoc($qry);
-
-                                $ses[] = $res['ses'];
-                                $descs[] = $res['descs'];
-                                $ratio[] = $res['ratio'];
-                            }
-                            else{
-                                $ses[] = 0;
-                                $descs[] = 0;
-                                $ratio[] = 0;
-                            }
-                        }
-                    }
-                }
-            }
-            
+        foreach($pse_data as $pse_row){            
             echo "
-            var ctx_ses".$_id." = document.getElementById('myChart_ses_".$_id."');
-            var ctx_des".$_id." = document.getElementById('myChart_des_".$_id."');
-            var ctx_rat".$_id." = document.getElementById('myChart_rat_".$_id."');
+            var ctx_ses".$pse_row['ATLETA']." = document.getElementById('myChart_ses_".$pse_row['ATLETA']."');
+            var ctx_des".$pse_row['ATLETA']." = document.getElementById('myChart_des_".$pse_row['ATLETA']."');
+            var ctx_rat".$pse_row['ATLETA']." = document.getElementById('myChart_rat_".$pse_row['ATLETA']."');
 
-            var myChart_ses".$_id." = new Chart(ctx_ses".$_id.", {
+            var myChart_ses".$pse_row['ATLETA']." = new Chart(ctx_ses".$pse_row['ATLETA'].", {
                 type: 'bar',
                 data: {
-                    labels:"; $res2 = mysqli_query($link, $sql2); $flag=1; echo '['; while($row = mysqli_fetch_assoc($res2)){if($flag==1) {echo '"'.date("d/m", strtotime($row['data'])).'"'; $flag=0;} else {echo ',"'.date("d/m", strtotime($row['data'])).'"';}} echo "],
+                    labels: [".implode($pse_row['DATAS'], ',')."],
                     datasets: [{
                         type: 'line',
                         label: 'PSE Sessão',
-                        data: [".implode($ses, ',')."],
+                        data: [".implode($pse_row['PSE_SES'], ',')."],
                         borderColor: 'rgba(0, 0, 0, 1)',
                         borderWidth: 2,
                         fill: false
@@ -193,7 +145,7 @@
                     {
                         type: 'bar',
                         label: 'PSE Sessão',
-                        data: [".implode($ses, ',')."],
+                        data: [".implode($pse_row['PSE_SES'], ',')."],
                         backgroundColor: 'rgba(255, 187, 0, 0.3)',
                         borderColor: 'rgba(255, 187, 0, 1)',
                         pointBackgroundColor: 'rgba(255, 187, 0, 1)',
@@ -232,14 +184,14 @@
                 }
             });
 
-            var myChart_des".$_id." = new Chart(ctx_des".$_id.", {
+            var myChart_des".$pse_row['ATLETA']." = new Chart(ctx_des".$pse_row['ATLETA'].", {
                 type: 'bar',
                 data: {
-                    labels:"; $res2 = mysqli_query($link, $sql2); $flag=1; echo '['; while($row = mysqli_fetch_assoc($res2)){if($flag==1) {echo '"'.date("d/m", strtotime($row['data'])).'"'; $flag=0;} else {echo ',"'.date("d/m", strtotime($row['data'])).'"';}} echo "],
+                    labels: [".implode($pse_row['DATAS'], ',')."],
                     datasets: [{
                         type: 'line',
                         label: 'PSE do Descanso',
-                        data: [".implode($descs, ',')."],
+                        data: [".implode($pse_row['PSE_DESCS'], ',')."],
                         borderColor: 'rgba(0, 0, 0, 1)',
                         borderWidth: 2,
                         fill: false
@@ -247,7 +199,7 @@
                     {
                         type: 'bar',
                         label: 'PSE do Descanso',
-                        data: [".implode($descs, ',')."],
+                        data: [".implode($pse_row['PSE_DESCS'], ',')."],
                         backgroundColor: 'rgba(255, 187, 0, 0.3)',
                         borderColor: 'rgba(255, 187, 0, 1)',
                         pointBackgroundColor: 'rgba(255, 187, 0, 1)',
@@ -286,14 +238,14 @@
                 }
             });
 
-            var myChart_rat".$_id." = new Chart(ctx_rat".$_id.", {
+            var myChart_rat".$pse_row['ATLETA']." = new Chart(ctx_rat".$pse_row['ATLETA'].", {
                 type: 'bar',
                 data: {
-                    labels:"; $res2 = mysqli_query($link, $sql2); $flag=1; echo '['; while($row = mysqli_fetch_assoc($res2)){if($flag==1) {echo '"'.date("d/m", strtotime($row['data'])).'"'; $flag=0;} else {echo ',"'.date("d/m", strtotime($row['data'])).'"';}} echo "],
+                    labels: [".implode($pse_row['DATAS'], ',')."],
                     datasets: [{
                         type: 'line',
                         label: 'PSE Ratio',
-                        data: [".implode($ratio, ',')."],
+                        data: [".implode($pse_row['PSE_RATIO'], ',')."],
                         borderColor: 'rgba(0, 0, 0, 1)',
                         borderWidth: 2,
                         fill: false
@@ -301,7 +253,7 @@
                     {
                         type: 'bar',
                         label: 'PSE Ratio',
-                        data: [".implode($ratio, ',')."],
+                        data: [".implode($pse_row['PSE_RATIO'], ',')."],
                         backgroundColor: 'rgba(255, 187, 0, 0.3)',
                         borderColor: 'rgba(255, 187, 0, 1)',
                         pointBackgroundColor: 'rgba(255, 187, 0, 1)',
@@ -339,10 +291,5 @@
         }
     ?>
     </script>
-
-   
-
-
 </body>
-
 </html>
